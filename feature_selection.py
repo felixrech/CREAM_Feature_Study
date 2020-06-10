517dfad7d8b8533b9ece1870dd819bbf8ec8bacd
Felix Rech <mail@felixrech.com>
2020-06-10 21:29:03 +0200

    Update forward selection & add feature combinations

--- feature_selection.py
+++ feature_selection.py
@@ -1,10 +1,16 @@
+import scipy
+import itertools
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.metrics import classification_report, plot_confusion_matrix
+
+
+line_del = ''.join(itertools.repeat('\r', 50))
 
 
 def normalize_features(features):
@@ -33,54 +39,85 @@ def evaluate_feature(X, y, metric, classifier, param_grid):
     return score, cv.best_estimator_
 
 
+def remove_features_by_dimension(features, dimension):
+    remove_list = []
+    for feature in features:
+        if features[feature].shape[1] > dimension:
+            remove_list.append(feature)
+    for feature in remove_list:
+        features.pop(feature)
+    return features
+
+
 def forward_selection(features, y, feature_eval, max_features=None,
                       limit_one_dimensional=False):
     # Set maximum max_features if not specified
     if max_features is None:
         max_features = len(features)
+
     # Limit features to one-dimensional features if specified
     if limit_one_dimensional:
-        for feature in features:
-            remove_list = []
-            if features[feature].shape[1] > 1:
-                remove_list.append(feature)
-        for feature in remove_list:
-            features.pop(feature)
+        features = remove_features_by_dimension(features, 1)
+
     # Set up variables
-    used_features = []
-    used_features_arr = np.empty((y.shape[0], 0))
-    current_score = 0
+    df = pd.DataFrame({'features': itertools.repeat([], len(features)),
+                       'score': itertools.repeat(0, len(features))})
+
     # Add a maximum of max_features features
     for n_features in range(max_features):
-        # Announce next round in forward selection
-        print(f"{n_features} Feature(s) selected with score "
-              f"{round(current_score, 3)}:\t{used_features}\n")
-        # Calculate score for all remaining unselected features
-        scores, estimators = [], []
-        for feature in features:
-            comb_features = np.hstack((used_features_arr, features[feature]))
-            score, estimator = feature_eval(comb_features, y)
-            scores.append(score)
-            estimators.append(estimator)
-        # Build dataframe from scores, then sort and print it
-        df = pd.DataFrame({'feature': list(features.keys()), 'score': scores,
-                           'estimator': estimators})
-        df = df.sort_values('score', ascending=False)
-        print(df[['feature', 'score']].to_string(index=False), '\n')
-        # Extract the best feature from dataframe
-        selected_feature, selected_score, selected_estimator = df.iloc[0]
-        # If selected feature would lead to better overall performance
-        if selected_score > current_score:
-            # Update variables for selection
-            current_score, current_estimator = selected_score, selected_estimator
-            used_features.append(selected_feature)
-            used_features_arr = np.hstack(
-                (used_features_arr, features[feature]))
-            features.pop(selected_feature)
-            print(f"Selecting feature:  {selected_feature}\n\n\n")
-        # Otherwise stop iteration
-        else:
-            print("No new feature selected (no score higher than current score)")
-            break
+        old_df = df
+        best = df.iloc[0]
+        df = pd.DataFrame(columns=['new', 'features', 'score', 'estimator'])
 
-    return current_estimator
+        # Calculate score for all remaining unselected features
+        for i, new_feature in enumerate(set(features) - set(best.features)):
+            print(f"{line_del}\tLooking at new feature: {new_feature}", end='')
+            new_features = best.features + [new_feature]
+            comb_features = np.hstack([features[f] for f in new_features])
+            score, estimator = feature_eval(comb_features, y)
+            df.loc[i] = (new_feature, new_features, score, estimator)
+
+        # Prepare for next iteration
+        df = df.sort_values('score', ascending=False)
+        new_best = df.iloc[0]
+        if new_best.score > best.score:
+            print(f"{line_del}\n{df[['new', 'score']]}\n\n"
+                  f"{len(new_best.features)} Feature(s) selected with score "
+                  f"{round(new_best.score, 3)}:\t{new_best.features}\n")
+        else:
+            print(f"{line_del}\n{df[['new', 'score']]}\n\n"
+                  "No new feature selected, returning")
+
+    return df.iloc[0]
+
+
+def feature_combinations(normed_features, y,  n_features,
+                         metric=None, classifier=None, param_grid={}):
+    # Set default values
+    if metric is None:
+        metric = 'f1_macro'
+    if classifier is None and param_grid == {}:
+        param_grid = {'n_neighbors': np.arange(1, 20)}
+    if classifier is None:
+        classifier = KNeighborsClassifier(n_jobs=-1)
+
+    df = pd.DataFrame(columns=['feature_combination', 'dimension', 'score'])
+    feature_combs = itertools.combinations(normed_features.keys(), n_features)
+    num_combs = int(scipy.special.comb(len(normed_features), n_features))
+    start = datetime.now()
+
+    for i, combination in enumerate(feature_combs):
+        time_remaining = (datetime.now() - start) / (i+1) * (num_combs - (i+1))
+        print(f"{line_del}Feature combination {i}/{num_combs}, "
+              f"time remaining: {time_remaining}", end='')
+
+        X = np.empty((y.shape[0], 0))
+        for feature in combination:
+            X = np.hstack((X, normed_features[feature]))
+            score, _ = evaluate_feature(X, y, metric, classifier, param_grid)
+            df.loc[i] = (combination, X.shape[1], score)
+
+    total_time = (datetime.now() - start)
+    print(f'{line_del}Calculations finished in {total_time}', end='')
+
+    return df.sort_values('score', ascending=False)
